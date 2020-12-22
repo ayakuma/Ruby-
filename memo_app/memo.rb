7f9313ab17b5a5d7f4a8aafe0a4b14163b962d5a
ayakuma <75723355+ayakuma@users.noreply.github.com>
2020-12-22 12:49:49 +0900

    second
    「ループ処理」、「3,メモを終了する」を追加。

--- memo_app/memo.rb
+++ memo_app/memo.rb
@@ -1,9 +1,9 @@
-require "csv"
+loop do
+  require "csv"
 
-puts "1(新規でメモを作成) 2(既存のメモ編集する)"
-memo_type = gets.to_i
+  puts "1(新規でメモを作成) 2(既存のメモ編集する) 3(メモを終了する)"
+  memo_type = gets.to_i
 
-while memo_type != 1 || 2 do
   if memo_type == 1
     puts "拡張子を除いたファイル名を入力してください"
     file_name = gets.chomp
@@ -15,22 +15,24 @@ while memo_type != 1 || 2 do
     CSV.open("#{file_name}.csv","w") do |csv|
       csv << ["#{memo}"]
     end
-    break
     
   elsif memo_type == 2
     puts "拡張子を除いたファイル名を入力してください"
     file_name = gets.chomp
 
-    puts "追記したい内容を入力してください"
+    puts "追記したいメモを入力"
+    puts "入力後、Ctrl + D で保存"
     memo = gets
     CSV.open("#{file_name}.csv","a") do |csv|
       csv << ["#{memo}"]
     end
+    
+  elsif memo_type == 3
+    puts "メモ機能を終了します。"
     break
-    
+
   else
-    puts "1 or 2(半角)で入力してください。"
-    memo_type = gets.to_i  
-    
+    puts "1 or 2 or 3で入力してください。"
   end
+
 end
